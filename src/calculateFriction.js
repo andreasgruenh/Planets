041b827008b67200666aaf5b865a9211c6e86fad
Andreas Roth <art_github@rtline.de>
2016-02-01 09:44:43 +0100

    Fixes friction

--- src/calculateFriction.js
+++ src/calculateFriction.js
@@ -7,10 +7,17 @@ module.exports = function calculateFrictionOnCicrcles(circles, _frictionConstant
 };
 
 function calculateFrictionOnCicrcle(circle) {
-  let frictionX = frictionConstant * circle.speedX;
-  let frictionY = frictionConstant * circle.speedY;
-  let accX = frictionX / circle.mass;
-  let accY = frictionY / circle.mass;
+  var accX, accY;
+
+  var lengthOfSpeedVektor = Math.sqrt(circle.speedX * circle.speedX + circle.speedY * circle.speedY);
+
+  var direction = {
+    x: circle.speedX / lengthOfSpeedVektor,
+    y: circle.speedY / lengthOfSpeedVektor
+  };
+
+  accX = frictionConstant * direction.x / circle.mass;
+  accY = frictionConstant * direction.y / circle.mass;
 
   let newXSpeed = circle.speedX - accX;
   circle.speedX = newXSpeed;
